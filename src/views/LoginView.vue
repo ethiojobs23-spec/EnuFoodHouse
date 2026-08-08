4ccdb69c37077927685fa2e2c4fe5789a9421c75
minteio <ethiojobs23@gmail.com>
2026-08-08 17:34:07 +0300

    Fix missing computed import in LoginView causing blank screen

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -28,7 +28,7 @@
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 import { useRouter } from 'vue-router';
 import { supabase } from '../services/supabase';
 
